e52575451a9902688a5d51662f72db137c53610f
Nikola Jovic <nikjovke1@gmail.com>
2019-05-10 02:19:26 +0200

    Gallery changed.

---
+++ js/gallery.js
@@ -0,0 +1,41 @@
+$(function()
+{
+    initialize();
+});
+
+function initialize()
+{
+    loadWatches(function(watches)
+    {
+        let imagesHtml = "";
+
+        for(let watch of watches)
+        {
+            imagesHtml += `<div class="col-xs-12 col-md-6 col-lg-3 p-1 pb-3">
+                                <div class="gallery-item">
+                                    <img src="../img/${watch.image}" alt="${watch.name}" class="img-fluid" />
+                                </div>
+                            </div>`;
+        }
+
+        $("#image-gallery").html(imagesHtml);
+    });
+}
+
+function loadWatches(callback)
+{
+    loadJson("../data/watches.json", callback);
+}
+
+function loadJson(path, successCallback)
+{
+    $.ajax({
+        url: path,
+        type: "GET",
+        dataType: "json",
+        success: function(data)
+        {
+            successCallback(data);
+        }
+    });
+}
